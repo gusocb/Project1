330e3ce0ec5aeb0de57d1d38aa142a71f73ef416
Gustavo <gustavocruzb@outlook.com>
2019-11-21 22:02:17 -0600

    jueves 21 noche

--- main.js
+++ main.js
@@ -2,48 +2,94 @@ let canvas = document.getElementById('game')
 let ctx = canvas.getContext('2d');
 canvas.width = 1000;
 canvas.height = 600;
-
 let interval;
 
 class Pokemon {
-    constructor(x,y,w,h,){
+    constructor(x,y,w,h,color){
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.life = 5;
+        this.color = color
     }
     draw(){
-        ctx.fillStyle = 'black'
+        ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.w, this.h)
     }
 }
 
-class Attack {
-    constructor(x,y,w,h,){
+class PowerBall {
+    constructor(x,y,w,h,color,direction){
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.speed = 10;
+        this.color = color;
+        this.direction =direction;
     }
-
+    
     draw(){
-        ctx.fillStyle = 'blue'
-        
+        ctx.fillStyle = this.color
+        this.x+=this.speed*this.direction;
+        ctx.fillRect(this.x,this.y,this.w,this.h)
     }
 }
 
 // INSTANCIAS
-let squirtle = new Pokemon (100,100,60,60);
+let powerBall = []; 
+let powerBall2 = []; 
+let squirtle = new Pokemon (100,300,60,60, 'blue');
+let charmander = new Pokemon (800,300,60,60, 'red')
+
+//COMPLEMENTOS
+
+//PODERES JUGADOR 1
+function makePowerBall1(){
+    powerBall.push(new PowerBall(150,290,40,40,'yellow',1))
+}
+
+function drawPowerBall1(){
+    powerBall.forEach(function(ele){
+        ele.draw();
+    })
+}
+
+//PODERES JUGADOR 2
+function makePowerBall2(){
+    powerBall2.push(new PowerBall(750,310,40,40, 'green',-1))
+}
+
+function drawPowerBall2(){
+    powerBall2.forEach(function(ele){
+        ele.draw();
+    })
+}
+
 
 //LO QUE SE VE
 
-function upload(){
+function refresh(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
     squirtle.draw();
-    
+    charmander.draw();
+    drawPowerBall1();
+    drawPowerBall2();
 
 }
 
-interval = setInterval(upload, 1000/60);
+//CONTROLES
+
+window.addEventListener('keydown', function(e) {
+    if(e.which == 90) {
+        makePowerBall1();
+    }
+    else if (e.which ==77){
+        makePowerBall2();
+    }
+});
+
+
+
+interval = setInterval(refresh, 1000/60);
